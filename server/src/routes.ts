8744489f33cda2787c4e7fad0ee6d86973db3de1
Luiz Casula <luizcasulaf@gmail.com>
2020-06-02 20:57:28 -0400

    get images

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,19 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
 
-routes.get('/', (request, response) => {
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*');
 
-    return response.json({ 'message': 'Hello World' });
+    const serializedItems = items.map(item => {
+        return {
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`
+        };
+    }); 
+    return response.json(serializedItems);
 });
-
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,12 @@
-import express from 'express';
+import express, { response } from 'express';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (resquest, response) => {
-    console.log('List users');
-    response.json(['Luiz', 'Antonio', 'Casula', 'Fornazzari']);
-});
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
