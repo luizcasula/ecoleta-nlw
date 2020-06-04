deb0602dce385724de9fb3f54a3f004b91a17bdb
Luiz Casula <luizcasulaf@gmail.com>
2020-06-03 22:44:53 -0400

    update database and fix error

--- server/src/database/seeds/default_items.ts
+++ server/src/database/seeds/default_items.ts
@@ -4,7 +4,7 @@ export async function seed(knex: Knex) {
     await knex('items').insert([
         { title: 'Lâmpadas', image: 'lampadas.svg' },
         { title: 'Pilhas e Baterias', image: 'baterias.svg' },
-        { title: 'Papéis e Papelão', image: 'papeis-papel.svg' },
+        { title: 'Papéis e Papelão', image: 'papeis-papelao.svg' },
         { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
         { title: 'Resíduos Orgânicos', image: 'organicos.svg' },
         { title: 'óleo de Cozinha', image: 'oleo.svg' }
