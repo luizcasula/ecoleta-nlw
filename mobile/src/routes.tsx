96b6b3b6b4f1043b80c3b60aaf4d4aec5b27e13b
Luiz Casula <luizcasulaf@gmail.com>
2020-06-05 13:39:23 -0400

    remove appbar

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -13,7 +13,7 @@ const AppStack = createStackNavigator();
 const Routes = () => {
   return (
     <NavigationContainer>
-      <AppStack.Navigator>
+      <AppStack.Navigator headerMode="none">
         <AppStack.Screen name="Home" component={Home} />
         <AppStack.Screen name="Points" component={Points} />
         <AppStack.Screen name="Detail" component={Detail} />
